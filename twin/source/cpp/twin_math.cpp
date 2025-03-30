d5cd6825f7544f6bf4d30195ac843d8b2001a76e
Yashin Artem <neokesha@yandex.ru>
2025-03-30 22:44:06 +0300

    Math

--- twin/source/cpp/twin_math.cpp
+++ twin/source/cpp/twin_math.cpp
@@ -1,3 +1,93 @@
 #include <XTL.h>
 #include <stl.h>
 #include "twin_math.h"
+#include <math.h>
+
+float __cdecl Vector3::GetLength(Vector3* vec) {
+	return sqrt(vec->x * vec->x + vec->y * vec->y + vec->z * vec->z);
+}
+
+void __cdecl Vector3::Scale(Vector3* dst, Vector3* src, float k) {
+	dst->x = src->x * k;
+	dst->y = src->y * k;
+	dst->z = src->z * k;
+}
+
+void __cdecl Vector3::Subtract(Vector3* dst, Vector3* a, Vector3* b) {
+	dst->x = a->x - b->x;
+	dst->y = a->y - b->y;
+	dst->z = a->z - b->z;
+}
+
+float Vector3::GetLengthXZ() {
+	return sqrt(this->x * this->x + this->z * this->z);
+}
+
+float Vector3::GetLengthSqr() {
+	return this->x * this->x + this->y * this->y + this->z * this->z;
+}
+
+void Vector3::Invert() {
+	this->x = -this->x;
+	this->y = -this->y;
+	this->z = -this->z;
+}
+
+float Vector3::DistanceSqr(Vector3* other) {
+	float dx = this->x - other->x;
+	float dy = this->y - other->y;
+	float dz = this->z - other->z;
+	return dx * dx + dy * dy + dz * dz;
+}
+
+float Vector3::Distance(Vector3* other) {
+	float dx = this->x - other->x;
+	float dy = this->y - other->y;
+	float dz = this->z - other->z;
+	return sqrt(dx * dx + dy * dy + dz * dz);
+}
+
+float Vector3::DotProduct(Vector3* other) {
+	return this->x * other->x + this->y * other->y + this->z * other->z;
+}
+
+void Vector3::CopyTo(Vector4* other) {
+	other->x = this->x;
+	other->y = this->y;
+	other->z = this->z;
+	other->w = 1.0f;
+}
+
+float Vector4::GetLengthSqr() {
+	return this->x * this->x + this->y * this->y + this->z * this->z + this->w * this->w;
+}
+
+float Vector4::GetLength() {
+	return sqrt(this->x * this->x + this->y * this->y + this->z * this->z + this->w * this->w);
+}
+
+void __cdecl Vector3::Transform(Vector3* a, Vector3* b, Matrix4* m) {
+	a->x = m->row3.x * b->z + m->row2.x * b->y + m->row1.x * b->x;
+	a->y = m->row3.y * b->z + m->row2.y * b->y + m->row1.y * b->x;
+	a->z = m->row3.z * b->z + m->row2.z * b->y + m->row1.z * b->x;
+}
+
+void __cdecl Vector4::Transform(Vector4* a, Vector4* b, Matrix4* m) {
+	a->x = m->row3.x * b->z + m->row2.x * b->y + m->row1.x * b->x + m->row4.x; //W = 1
+	a->y = m->row3.y * b->z + m->row2.y * b->y + m->row1.y * b->x + m->row4.y; //W = 1
+	a->z = m->row3.z * b->z + m->row2.z * b->y + m->row1.z * b->x + m->row4.z; //W = 1
+}
+
+void Vector4::Reset() {
+	this->y = 0.0f;
+	this->z = 0.0f;
+	this->x = (float)((unsigned int)this->x & 0xfffffcff | 0xff);
+	this->w = 1.0f;
+}
+
+void Vector4::CopyTo(Vector4* other) {
+	other->x = this->x;
+	other->y = this->y;
+	other->z = this->z;
+	other->w = 1.0f;
+}
--- twin/source/cpp/twin_math.h
+++ twin/source/cpp/twin_math.h
@@ -1,17 +1,49 @@
 #pragma once
 
+class Matrix4;
+class Vector2;
+class Vector3;
+class Vector4;
+
 class Vector2 {
 public:
 	float x;
 	float y;
 };
 
-class Vector4{
+class Vector3 {
+public:
+	float x;
+	float y;
+	float z;
+	
+	float GetLengthSqr();
+	float GetLengthXZ();
+	void Invert();
+	float Distance(Vector3* other);
+	float DistanceSqr(Vector3* other);
+	float DotProduct(Vector3* other);
+	void CopyTo(Vector4* other);
+	
+	static float __cdecl GetLength(Vector3* vec);
+	static void __cdecl Scale(Vector3* vec, Vector3* src, float k);
+	static void __cdecl Subtract(Vector3* dst, Vector3* a, Vector3* b);
+	static void __cdecl Transform(Vector3* a, Vector3* b, Matrix4* m);
+};
+
+class Vector4 {
 public:
 	float x;
 	float y;
 	float z;
 	float w;
+	
+	float GetLength();
+	float GetLengthSqr();
+	void Reset();
+	void CopyTo(Vector4* other);
+	
+	static void __cdecl Transform(Vector4* a, Vector4* b, Matrix4* m);
 };
 
 class Matrix4 {
