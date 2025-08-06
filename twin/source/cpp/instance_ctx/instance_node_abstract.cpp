08c2a015afc0884c3e8c3e0823771c578efcc3e0
Yashin Artem <neokesha@yandex.ru>
2025-08-06 03:05:09 +0300

    Virtual part of Instance Node Abstract

--- twin/source/cpp/instance_ctx/instance_node_abstract.cpp
+++ twin/source/cpp/instance_ctx/instance_node_abstract.cpp
@@ -4,7 +4,8 @@
 
 InstanceNodeAbstract::InstanceNodeAbstract() {
 	ctx = 0;
-	a = 0;
+	a1 = 0;
+	a2 = 0;
 	b = 0;
 	time = 0;
 	ptr = 0;
@@ -23,4 +24,31 @@ void InstanceNodeAbstract::SetCtx(InstanceContext* ctx) {
 	if (this->ctx == 0) {
 		this->ctx = ctx;
 	}
+}
+
+bool InstanceNodeAbstract::IsA() {
+	return true;
+}
+
+void InstanceNodeAbstract::EmptyFunction(int param) {
+	
+}
+
+void InstanceNodeAbstract::UpdateTime(UnkTimePack* time) {
+	this->time = time->fields[0].time1;
+}
+
+bool InstanceNodeAbstract::Step(UnkTimePack* newTime) {
+	if ((newTime->fields[0].flags & 1) != 0) {
+		if (((~a1 & 1) == 0) && (time != 0)) {
+			a1 = a1 & 0xfe;
+			return true;
+		}
+		time = newTime->fields[0].time1;
+	}
+	return true;
+}
+
+void InstanceNodeAbstract::EmptyFunction2() {
+
 }
--- twin/source/cpp/instance_ctx/instance_node_abstract.h
+++ twin/source/cpp/instance_ctx/instance_node_abstract.h
@@ -1,5 +1,6 @@
 #pragma once
 #include <twin_base.h>
+#include <time\unk_time_pack.h>
 #include "instance_context.h"
 
 class InstanceNodeAbstract : public TwinBase {
@@ -10,9 +11,17 @@ public:
 	virtual void ReleaseCtxPtr(InstanceContextSmartPtr ptr);
 	virtual ~InstanceNodeAbstract();
 	virtual void SetCtx(InstanceContext* ctx);
+	virtual bool IsA();
+	virtual int GetIndex() = 0;
+	virtual void EmptyFunction(int param);
+	virtual void UpdateTime(UnkTimePack* time);
+	virtual bool Step(UnkTimePack* time);
+	virtual void EmptyFunction2();
+	virtual int GetBuilderIndex() = 0;
 
 	InstanceContext* ctx;
-	short a;
+	unsigned char a1;
+	unsigned char a2;
 	short b;
 	int time;
 	InstanceNodeAbstract* ptr;
